46b4c31606786a4152fb35392e61cf2dac7672f8
omar-nav <omar.navarro@g.austincc.edu>
2018-09-14 12:07:22 -0500

    first commit

---
+++ routes/books.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/Book')
+
+router.get('/', (req, res, next) => {
+  const { search } = req.query
+  if (search) {
+    Book.find({ title: { $regex: search, $options: 'i' } })
+      .then(books => {
+        res.render('books-list', { books })
+      }).catch(e => next(e))
+  } else {
+    Book.find()
+      .then(books => {
+        res.render('books-list', { books })
+      })
+  }
+})
+router.get('/detail/:id', (req, res, next) => {
+  const { id } = req.params
+  Book.findById(id)
+    .then(book => {
+      res.render('book-detail', book)
+    }).catch(e => {
+      console.log(e)
+      next(e)
+    })
+})
+
+
+module.exports = router
