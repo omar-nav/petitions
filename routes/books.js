a52e2a8c8534cb2081807b7f3eb34a3838e6ff28
omar-nav <omar.navarro@g.austincc.edu>
2018-09-14 16:58:37 -0500

    finalized petitions

--- routes/books.js
+++ routes/books.js
@@ -26,6 +26,42 @@ router.get('/detail/:id', (req, res, next) => {
       next(e)
     })
 })
+//post
+router.get('/new', (req, res, next) => {
+  res.render('book-form')
+})
+router.post('/new', (req, res, next) => {
+  Book.create(req.body)
+    .then(book => {
+      res.redirect('/books')
+    }).catch(e => next(e))
+})
 
+//update
+router.get('/edit/:id', (req, res, next) => {
+  const { id } = req.params
+  Book.findById(id)
+    .then(book => {
+      res.render('book-edit-form', book)
+    }).catch(e => next(e))
+})
+
+router.post('/edit/:id', (req, res, next) => {
+  const { id } = req.params
+  Book.findByIdAndUpdate(id, { $set: req.body }, { new: true })
+    .then(book => {
+      console.log(book)
+      res.redirect(`/books/detail/${id}`)
+    })
+})
 
 module.exports = router
+
+// crear un nuevo modelo que se llama review
+
+// enlistar nuevas rutas 
+
+
+
+
+
